d1201f27383f65800b54bf1d7ea64193713855fc
tralwdwd <79791517+tralwdwd@users.noreply.github.com>
2025-09-24 18:31:42 +0100

    chore: update template for package update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { definePlugin } from "@readit-mod/plugins";
 export default definePlugin({
     name: "ReadIt Plugin",
     description: "Provide a plugin description.",
+    id: "my-plugin",
     version: "1.0.0",
     onLoad: (ctx) => {
         // Your code here
